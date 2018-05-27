a5965b84709a2238b16e0c8e4ef111bdfb2f014d
Vinay <vinay.raghuvanshi19@gmail.com>
2018-05-27 17:14:39 +0530

    Added header for the site

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   root 'dashboard#home'
 
-  resources :tasks
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :tasks do
+    collection do
+      get :my_tasks
+    end
+  end
+
 end
